8e51293553d76ca4c387b549d2a51711901ae997
aman <tomaraman50@gmail.com>
2026-02-04 15:39:38 +0530

    feat: add initial CategoryManagement page component

---
+++ src/pages/CategoryManagement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CategoryManagement = () => {
+  return (
+    <div className='p-4'>
+        <h2 className='text-2xl font-bold mb-4'>Category Management</h2>
+        <p>Category management functionality will be implemented here.</p>
+    </div>
+  )
+}
+
+export default CategoryManagement
